a6127850545f0cbff390362edc56feffcc5989f1
Amaan <amaan.jamil@keatechinc.com>
2023-06-12 11:31:57 -0400

    clang format + actions

--- main.cpp
+++ main.cpp
@@ -4,38 +4,34 @@
 
 class Base {
 public:
-    virtual void foo() = 0;
+  virtual void foo() = 0;
 };
 class Child1 : public Base {
 public:
-    void foo() override { 
-        std::cout << "Child 1" << std::endl;
-    }
+  void foo() override { std::cout << "Child 1" << std::endl; }
 };
 class Child2 : public Base {
 public:
-    void foo() override { 
-        std::cout << "Child 2" << std::endl; 
-    }
+  void foo() override { std::cout << "Child 2" << std::endl; }
 };
 
 int main() {
-    std::unique_ptr<Base> test;
+  std::unique_ptr<Base> test;
 
-    int selector;
+  int selector;
 
-    std::cin >> selector;
+  std::cin >> selector;
 
-    switch (selector) {
-    case 1:
-        test = std::make_unique<Child1>();
-        break;
-    case 2:
-        test = std::make_unique<Child2>();
-        break;
-    }
+  switch (selector) {
+  case 1:
+    test = std::make_unique<Child1>();
+    break;
+  case 2:
+    test = std::make_unique<Child2>();
+    break;
+  }
 
-    test->foo();
+  test->foo();
 
-    return 0;
+  return 0;
 }
